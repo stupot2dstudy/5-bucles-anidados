80667026be0debe3f148d463df794e87f18c6b85
stupot2dstudy <stupot2dstudy@gmail.com>
2023-07-20 08:48:53 +0100

    exercise completed

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ const catalogo = [
   "barbacoa",
   "tropical",
 ];
+
+function getRandomPizzaPair() {
+  const pizzaPairs = [];
+  for (let i = 0; i < catalogo.length; i++) {
+    for (let j = i + 1; j < catalogo.length; j++) {
+      pizzaPairs.push([catalogo[i], catalogo[j]]);
+    }
+  }
+  return pizzaPairs;
+}
+
+const pizzaPairs = getRandomPizzaPair();
+pizzaPairs.forEach((pair) => console.log(pair[0], "and", pair[1]));
